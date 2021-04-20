bfb958ef1bb9e8d775fb8d7b4ad0f507eb7b2884
@sebas.davila.garcia <sebas.davila.garcia@gmail.com>
2021-04-20 10:47:42 -0500

    Eliminar todo y boton para seleccionar todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,20 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
   inputValue: any;
+  idgenerate:number;
   todos: Array<String>=[];
-  constructor() { }
+  checkedAll:Boolean=false;
+  constructor() { this.idgenerate=1;
+    
+  }
 
   ngOnInit(): void {
   }
 
   sendTodo(){
-    
-    
     if(this.inputValue.trim()!==""){
+
       this.todos.push(this.inputValue);
       this.inputValue="";
     }
     else{alert("Ingrese un valor valido")}
+  }
+  deleteTodo(todo:any){
+  
+  this.todos = this.todos.filter(tod => tod!=todo)
+  }
+  seleccionartodo(){
+    this.checkedAll=!this.checkedAll;
   }
 
 }
